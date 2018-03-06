6b626c3c452ce75923b5f76075df71bf8d6a49d2
fayemcc <rosieandpip@gmail.com>
2018-03-06 15:04:42 +0000

    added the about rango page

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there Partner!")
+    return HttpResponse("Rango says hey there Partner! \n\n For more information, please visit http://127.0.0.1:8000/rango/about/")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page!")
